92dca3fc64e0a30b9b53f24db639a42b402aa25a
Siva-Kumar10 <sivakumar9010.kala@gmail.com>
2026-08-29 10:01:34 +0000

    Adding Jenkins script

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#!/bin/bash
+
+echo "================================="
+echo " Jenkins GitHub CI/CD Demo"
+echo "================================="
+echo "Build triggered successfully!"
+echo "Current date and time:"
+date
+echo "Hostname:"
+hostname
+echo "================================="
