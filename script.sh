99941d06abd5e7350c40acdfd082a03faa3a4a6d
Siva-Kumar10 <sivakumar9010.kala@gmail.com>
2026-08-29 10:43:18 +0000

    Updated Jenkins file

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@
 echo "================================="
 echo " Jenkins GitHub CI/CD Demo"
 echo "================================="
-echo "Build triggered successfully!"
+echo "Build triggered automatically from Git Hub!"
 echo "Current date and time:"
 date
 echo "Hostname:"
